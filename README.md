d7fed4e077512bc5c35adefc46f501cb433e9b61
Ali Thompson <alithom123@gmail.com>
2019-03-16 20:02:23 -0500

    remove old files

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 This is a Command Line Interface application made with NodeJS & MySQL. It was used to learn how to use the two technologies together. It is a simplified ecommerce platform where you can pick purchase products. 
 
 I created the app while attending the University of Texas' Full Stack Development Bootcamp.  
+
+Here is a screenshot of the code
+
+![alt text](demo.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,5 @@
-var inquirer = require("inquirer");
 var mysql = require("mysql");
-
+var inquirer = require("inquirer");
 
 console.log("bamazonCustomer.js is running");
 
@@ -11,73 +10,149 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
 
-async function getProductList() {
-    var choices = [];
-    choices = await connection.query('SELECT * FROM products', function (error, results, fields) {
-        if (error) throw error;
-        var arr = [];
-        results.forEach(function (product) {
-            arr.push(`${product.product_name} - ${product.price}`);
-            // console.log(choices);
+// the function for the app
+var start = function () {
+    // variables we'll need
+    var productArray;
+    var productChoiceQuestion;
+    var selectedProduct;
+    var selectedProductIndex;
+    var buyQuantity;
+
+    // Print initial list of products.
+    getProductList()
+        .then(products => {
+            // Save products in local variable.
+            productArray = products;
+
+            // Print Product List.
+            printProductList(products);
+
+            // Create array of only product names for prompting user.
+            var productNames = [];
+            products.forEach(function (eachProduct) {
+                productNames.push(eachProduct.product_name);
+            });
+
+            // Create inquirer prompt for product.
+            productChoiceQuestion = {
+                name: "productChoice",
+                type: "rawlist",
+                message: "Which product would you like to buy?",
+                choices: productNames
+            };
+
+            inquirer.prompt([productChoiceQuestion])
+                .then(answer => {
+                    // console.log("You chose", answer);
+
+                    // Reset selected product 
+                    selectedProductIndex = null;
+                    selectedProduct = null;
+
+                    // Find which product they chose.
+                    for (let i = 0; i < products.length; i++) {
+                        // console.log("looping");
+                        if (answer.productChoice === products[i].product_name) {
+                            selectedProduct = products[i];
+                            selectedProductIndex = i;
+                            // console.log("selectedProduct:", selectedProduct);
+                        }
+                    }
+
+                    // Create inquirer prompt for quantity to sell.
+                    buyQuantityQuestion = {
+                        name: "buyQuantity",
+                        type: "input",
+                        message: "How many would you like to buy?"
+                    };
+
+                    inquirer.prompt(buyQuantityQuestion)
+                        .then(quantityAnswer => {
+                            buyQuantity = parseInt(quantityAnswer.buyQuantity, 10);
+                            // console.log("You want to buy quantity", buyQuantity);
+                            if (isNaN(buyQuantity)) {
+                                console.log("You need to enter a valid number");
+                            } else {
+                                // console.log("selectedProduct:", selectedProduct);
+                                let newQuantity = selectedProduct.stock_quantity - buyQuantity;
+                                sellProduct(selectedProduct, newQuantity)
+                                    .then(boolresult => {
+                                        console.log("The total cost of your purchase was " + buyQuantity * selectedProduct.price);
+                                        start();
+                                    })
+                                    .catch(err => {
+                                        console.log(err);
+                                    });
+                            }
+                        });
+
+
+                })
+
+                .catch(err => {
+                    console.log(err);
+                });
+
         });
-        return choices;
-    });
-    console.log(choices);
-    // console.log("Choices I found ", choices);
-    return choices;
+
+    /* Start bidding loop for customer. */
 };
 
-
-
-
-var start = async function () {
-    connection.connect();
-    // getProductList();
-    // return;
-    var productChoiceQuestion = {
-        message: "Which product would you like to buy?",
-        type: "list",
-        name: "productChoice",
-        choices: getProductList()
-    };
-
-    inquirer.prompt([productChoiceQuestion, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
-        .then(answers => {
-            console.log("You chose", answers);
+function sellProduct(product, newQuantity) {
+    // console.log("product:", product);
+    // console.log("newQuantity:", newQuantity);
+    return new Promise((resolve, reject) => {
+        connection.query('UPDATE products SET stock_quantity =' + newQuantity + ' WHERE item_id =' + product.item_id, function (error, results, fields) {
+            if (error)
+                reject(error);
+            else
+                resolve(true);
         });
-    getProductList();
-
-    // Print List of Products
-    let products = await connection.query('SELECT * FROM products', function (error, results, fields) {
-        if (error) throw error;
-        return results;
-        // console.log(
-        //     `
-        //         Product Id | Product Name | Department | Price | Quantity
-        //         ___________________________________________
-        //         `);
-        // results.forEach(function (product) {
-        //     console.log(`${product.item_id} ${product.product_name} ${product.department_name}   ${product.price}    ${product.stock_quantity}`);
-        // });
     });
-    // console.log(products);
-    return;
+}
 
-    // var productChoiceQuestion = {
-    //     message: "Which product would you like to buy?",
-    //     type: "list",
-    //     name: "productChoice",
-    //     choices: getProductList()
-    // };
+function getProductList() {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM products', function (error, results, fields) {
+            if (error) reject(error);
 
-    // inquirer.prompt([productChoiceQuestion, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
-    //     .then(answers => {
-    //         console.log("You chose", answers);
-    //     });
-
+            let productArray = [];
+            results.forEach(function (eachProduct) {
+                let p = {
+                    item_id: eachProduct.item_id,
+                    product_name: eachProduct.product_name,
+                    department_name: eachProduct.department_name,
+                    price: eachProduct.price,
+                    stock_quantity: eachProduct.stock_quantity
+                }
+                // console.log(results);
+                productArray.push(p);
+            });
+            resolve(productArray);
+        });
+    });
 };
 
-start();
-
-// Use async await.
+function printProductList(products) {
+    console.log(
+        `
+    =========================================================
+    Item_id Product_Name Department_Name Price Stock_Quantity
+    =========================================================
+    `);
+    products.forEach(p => {
+        console.log(`${p.item_id} ${p.product_name} ${p.department_name} ${p.price} ${p.stock_quantity}`);
+    });
+    console.log(
+        `
+    =========================================================
+    `);
+}
